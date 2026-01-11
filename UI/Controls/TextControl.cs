c0e809bdb47b1f54a0fd36e0920a82e2002100d6
Atan Games <atangames3@gmail.com>
2026-01-11 17:25:40 +0100

    f

--- UI/Controls/TextControl.cs
+++ UI/Controls/TextControl.cs
@@ -4,11 +4,18 @@ using AtanUtils.UI.Data;
 using AtanUtils.UI.Interfaces;
 using TMPro;
 using UnityEngine;
+using UnityEngine.EventSystems;
 
 namespace AtanUtils.UI.Controls
 {
-    public class TextControl : MonoBehaviour, IPaletteAware
+    public class TextControl : MonoBehaviour, IPaletteAware, IPointerEnterHandler, IPointerExitHandler
     {
+        public enum HoverMode
+        {
+            None,
+            Underline,
+        }
+        
         private TMP_Text _text;
 
         public string Text
@@ -19,6 +26,9 @@ namespace AtanUtils.UI.Controls
 
         public PaletteKey defaultColor = PaletteKey.TextPrimary;
         public FontKey fontKey = FontKey.Main;
+        public HoverMode hoverMode = HoverMode.None;
+        
+        private bool _isHovered = false;
         
         private void Awake()
         {
@@ -36,6 +46,32 @@ namespace AtanUtils.UI.Controls
             _text ??= GetComponent<TMP_Text>();
             _text.color = defaultColor.GetColor();
             _text.font = fontKey.GetFont();
+        }
+
+        public void OnPointerEnter(PointerEventData eventData)
+        {
+            if (_isHovered)
+                return;
+            
+            _isHovered = true;
+            
+            if (hoverMode == HoverMode.Underline)
+            {
+                _text.text = "<u>" + _text.text + "</u>";
+            }
+        }
+
+        public void OnPointerExit(PointerEventData eventData)
+        {
+            if (!_isHovered)
+                return;
+            
+            _isHovered = false;
+            
+            if (hoverMode == HoverMode.Underline)
+            {
+                _text.text = _text.text.Replace("<u>", "").Replace("</u>", "");
+            }
         }
     }
 }
